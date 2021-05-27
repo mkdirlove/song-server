0bfbf05dcc9f18a3cafceb064e2f068b81e03c26
agiletelescope <agiletelescope@gmail.com>
2021-05-27 21:17:17 +0530

    register blueprints

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
 from flask import Flask
+from flask import jsonify
 
 from shared.configs import IS_DEBUG_MODE
 from extensions.extinit import init_extensions
@@ -9,6 +10,24 @@ def create_app(is_testing=False):
     flask_app = Flask(__name__)
     # Init jwt, mongodb and other external plugins
     init_extensions(flask_app, is_testing)
+
+    # Blueprints importing should be done
+    # after db init is complete
+    from services.users.routes import bp_user
+    from services.songs.routes import bp_songs
+
+    # Register blueprints
+    flask_app.register_blueprint(bp_user)
+    flask_app.register_blueprint(bp_songs)
+
+    # Generic Error Handlers
+    @flask_app.errorhandler(400)  # Bad request
+    @flask_app.errorhandler(401)  # Un-authorized
+    @flask_app.errorhandler(404)  # Not found
+    @flask_app.errorhandler(405)  # method not allowed
+    @flask_app.errorhandler(500)  # Internal server error
+    def error_handler(e):
+        return jsonify({'code': e.code, 'message': str(e)}), e.code
 
     return flask_app
 
