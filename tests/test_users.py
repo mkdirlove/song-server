de607d4a743686c0a08002deba7e66f0e0856dae
agiletelescope <agiletelescope@gmail.com>
2021-05-28 23:01:12 +0530

    major change to tests, they now dont recompute the expected code but use a precomputed code,

--- tests/test_users.py
+++ tests/test_users.py
@@ -7,44 +7,36 @@ from shared.utils import search_users_list
 
 
 @pytest.mark.parametrize(
-    "username, password",
+    "username, password, expected_code",
     [
-        ('username', None),             # Password none
-        (None, 'password'),             # Username none
-        (None, None),                   # Data none, leads to empty headers
-        ('', ''),  # Empty headers
+        ('username', None, 400),             # Password none
+        (None, 'password', 400),             # Username none
+        (None, None, 400),                   # Data none, leads to empty headers
+        ('', '', 401),                       # Empty headers
 
-        ('admin', 'admin'),             # Valid admin login
-        ('admin1', 'admin'),            # Invalid admin login
-        ('admin', '12345'),             # Invalid password
+        ('admin', 'admin', 200),             # Valid admin login
+        ('admin1', 'admin', 401),            # Invalid admin login
+        ('admin', '12345', 401),             # Invalid password
 
-        ('random_user', '12345'),       # User doesn't exist
-        ('Barbara Rocha', 'password'),  # Valid user login
-        ('Barbara Rocha', '12345678'),  # Invalid user login
+        ('random_user', '12345', 401),       # User doesn't exist
+        ('Barbara Rocha', 'password', 200),  # Valid user login
+        ('Barbara Rocha', '12345678', 401),  # Invalid user login
 
         # TODO, add different data types data, and different length of data
         # ('abc'*1000, 'password'*1000)
         # (100, {'one': 1})
     ]
 )
-def test_user_login(app, users_data, username, password):
+def test_user_login(app, username, password,
+                    expected_code, is_ignore_assertion=False):
 
     # Make the login request
     headers = {'username': username, 'password': password}
     headers = remove_none_keys(headers)
     request = app.post('/login', headers=headers)
 
-    # Expected response
-    expected_code = 200
-    # Search if user exists
-    user = search_users_list(username, users_data)
-    if user is None or \
-            not check_password_hash(user.password, password):
-        expected_code = 401
-    if username is None or password is None:
-        expected_code = 400
-
-    assert request.status_code == expected_code
+    if not is_ignore_assertion:
+        assert request.status_code == expected_code
     if request.status_code == 200:
         # Valid request,
         # Ensure that the response has a access key
@@ -57,44 +49,50 @@ def test_user_login(app, users_data, username, password):
 
 
 @pytest.mark.parametrize(
-    "request_username, request_password, username, password, dob",
+    "request_username, request_password, username, password, dob, expected_code",
     [
         # Invalid login
         # Leads to failure in generation of access_key
         # then leads to jwt failure when adding new user
-        (None, None, None, None, None),
-        ('admin', 'wrong_password', None, None, None),
-        ('wrong_username', 'wrong_password', None, None, None),
-        ('admin', 'wrong_password', 'new_user', 'password', 12345),
+        (None, None, None, None, None, 401),
+        ('admin', 'wrong_password', None, None, None, 401),
+        ('wrong_username', 'wrong_password', None, None, None, 401),
+        ('admin', 'wrong_password', 'new_user', 'password', 12345, 401),
 
         # Valid admin login and access key generation
         # Invalid body request
-        ('admin', 'admin', None, None, None),               # No body
-        ('admin', 'admin', None, 'password', 12345),        # No username key
-        ('admin', 'admin', 'new_user', None, 12345),        # No password key
-        ('admin', 'admin', 'new_user', 'password', None),   # No dob key
+        ('admin', 'admin', None, None, None, 400),               # No body
+        ('admin', 'admin', None, 'password', 12345, 400),        # No username key
+        ('admin', 'admin', 'new_user', None, 12345, 400),        # No password key
+        ('admin', 'admin', 'new_user', 'password', None, 400),   # No dob key
 
         # Valid admin login and access key generation
         # Invalid user details
-        ('admin', 'admin', '', '', ''),
-        ('admin', 'admin', '', 'password', 12345),
-        ('admin', 'admin', 'a', 'b', 12345),
-        ('admin', 'admin', 'abc', 'abc', 12345),
+        ('admin', 'admin', '', '', '', 400),
+        ('admin', 'admin', '', 'password', 12345, 400),
+        ('admin', 'admin', 'a', 'b', 12345, 400),
+        ('admin', 'admin', 'abc', 'abc', 12345, 400),
 
         # Duplicate user
-        ('admin', 'admin', 'Donald Lee', 'password', 12345),
+        ('admin', 'admin', 'Donald Lee', 'password', 12345, 400),
 
-        # Valid request
-        ('admin', 'admin', 'new_user', 'password', 12345),
+        # Valid request, admin adding a new user
+        ('admin', 'admin', 'new_user', 'password', 12345, 200),
+
+        # Invalid request, non admin trying to add a new user
+        ('Barbara Rocha', 'password', 'new_user', 'password', 12345, 400),
+        ('Barbara Rocha', 'wrong_password', 'new_user', 'password', 12345, 401),
+
+        # TODO, add different data types data, and different length of data
     ]
 )
-def test_add_new_user(app, users_data,
-                      request_username, request_password,
-                      username, password, dob):
+def test_add_new_user(app, request_username, request_password,
+                      username, password, dob, expected_code):
 
     # Make the login request
-    access_key = test_user_login(app, users_data,
-                                 request_username, request_password)
+    access_key = test_user_login(
+        app, request_username, request_password,
+        expected_code, is_ignore_assertion=True)
 
     # Make request to add new user
     headers = None
@@ -103,30 +101,6 @@ def test_add_new_user(app, users_data,
     body = {'username': username, 'password': password, 'dob': dob}
     body = remove_none_keys(body)
     request = app.post('/add_user', json=body, headers=headers)
-
-    # Expected response
-    expected_code = 200
-
-    if username is None or \
-            password is None or dob is None:
-        expected_code = 400
-    if len(username or '') < MIN_USERNAME_LEN:
-        expected_code = 400
-    if len(password or '') < MIN_USER_PASSWORD_LEN:
-        expected_code = 400
-
-    if headers is None:
-        expected_code = 401
-
-    user = search_users_list(request_username, users_data)
-    new_user = search_users_list(username, users_data)
-    if user is None:
-        expected_code = 401
-    elif not user.can_add_users():
-        expected_code = 400
-
-    if new_user is not None:
-        expected_code = 400
 
     assert request.status_code == expected_code
     if request.status_code != 200:
